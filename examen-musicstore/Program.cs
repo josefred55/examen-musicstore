e34885a3b49c6b3f1993686fee904320e775c6df
Freddy Arévalo <freddyarevalo2006@gmail.com>
2026-05-11 09:37:52 +0200

    Lista, busqueda y fichero

--- examen-musicstore/Program.cs
+++ examen-musicstore/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using examen_musicstore;
+
+//apartado 2.3
+static void guardarAlbumes(List<Album> albumes, String ruta) {
+    StreamWriter flujoSalida = File.CreateText(ruta);
+    foreach(Album album in albumes)
+    {
+        flujoSalida.WriteLine(album.Titulo + ";" + album.Artista + ";" +
+            album.Anyo + ";" + album.Disponible); 
+    }
+    Console.WriteLine("Albumes guardados correctamente");
+    flujoSalida.Close();
+}
+static void Main(string[] args)
+{
+    //Apartado 2.1
+    //A
+    List<Album> albumes = new List<Album>();
+    albumes.Add(new Album("Plastic Beach", "Gorillaz", 2010, true));
+    albumes.Add(new Album("Thriller", "Michael Jackson", 1982, false));
+    albumes.Add(new Album("Muerte", "Canserbero", 2012, true));
+
+    //B
+    foreach(Album album in albumes)
+    {
+        Console.WriteLine(album.ToString());
+    }
+
+    //C
+    foreach(Album album in albumes)
+    {
+        if (album.Artista.Contains("Metallica"))
+        {
+            Console.WriteLine(album.ToString());
+        }
+    }
+
+    //apartado 2.2
+    DateTime fecha = DateTime.Now;
+    Console.WriteLine("Fecha actual: " + fecha.ToShortDateString());
+
+    guardarAlbumes(albumes, "albumes.txt");
+}
+
+Main(args);
